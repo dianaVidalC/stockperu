4034c77de6ed05ba5668d4d178358e7b84eef7e6
Erika Vidal <erika.vidal@harrison.com.pe>
2020-08-14 12:32:32 -0500

    Updates

---
+++ precache-manifest.6805e8015b71ae950e79afe4e2957e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79ee67b0fde64a07f6154e5109be7d3a",
+    "url": "/stockperu/index.html"
+  },
+  {
+    "revision": "2c721562599a4d094ee3",
+    "url": "/stockperu/static/css/main.936473d5.chunk.css"
+  },
+  {
+    "revision": "7215dc1e4c19583202db",
+    "url": "/stockperu/static/js/2.92e59f10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stockperu/static/js/2.92e59f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c721562599a4d094ee3",
+    "url": "/stockperu/static/js/main.a2fdf90a.chunk.js"
+  },
+  {
+    "revision": "7df2a9bb1d1195754fa0",
+    "url": "/stockperu/static/js/runtime-main.bbac5ba3.js"
+  }
+]);
